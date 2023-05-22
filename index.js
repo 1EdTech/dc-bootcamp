5f2eb5bde25bd4bc882e3f0dab98daba3e43b38a
Xavi Aracil <xaracil@1edtech.org>
2023-05-22 14:41:19 +0200

    Added verification

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {cryptosuite as eddsa2022CryptoSuite} from
   '@digitalbazaar/eddsa-2022-cryptosuite';
 import dataIntegrity from '@digitalbazaar/data-integrity-context';
 
+import { verifyCredential } from '@digitalbazaar/vc';
 
 import context   from './context.js';
 
@@ -14,8 +15,7 @@ import context   from './context.js';
 const unsignedCredential = {
     '@context': [
       "https://www.w3.org/2018/credentials/v1",
-      "https://purl.imsglobal.org/spec/ob/v3p0/context/ob_v3p0.jsonld",
-      "https://www.w3.org/2018/credentials/v1"
+      "https://purl.imsglobal.org/spec/ob/v3p0/context/ob_v3p0.jsonld"
     ],
     id: "http://example.com/credentials/3527",
     type: [
@@ -74,6 +74,28 @@ loader.addStatic(
 loader.addStatic(
   "https://purl.imsglobal.org/spec/ob/v3p0/context/ob_v3p0.jsonld", context
 )
+loader.addStatic(
+  controller + '#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi',
+  {
+    "@context": "https://w3id.org/security/multikey/v1",
+    type: 'Multikey',
+    controller,
+    id: controller + '#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi',
+    publicKeyMultibase: 'z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi',
+  }
+);
+loader.addStatic(
+  controller, {
+  "@context": [
+    "https://www.w3.org/ns/did/v1",
+    "https://w3id.org/security/multikey/v1"
+  ],
+  "id": controller,
+  "assertionMethod": [
+    controller + '#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi'
+
+  ]
+});
 
 const documentLoader = loader.build();
 
@@ -85,3 +107,18 @@ const signedCredential = await jsigs.sign(unsignedCredential, {
 });
 
 console.log(JSON.stringify(signedCredential, null, 2));
+
+// verification
+
+const verSuite = new DataIntegrityProof({
+  controller,
+  cryptosuite: eddsa2022CryptoSuite
+});
+
+const verified = await verifyCredential({
+  credential: signedCredential,
+  documentLoader: documentLoader,
+  suite: [verSuite]
+})
+
+console.log(verified);
