8b0345808cc94441465fbe6693a1aa2dcae0658f
Xavi Aracil <xaracil@1edtech.org>
2023-05-29 10:47:03 +0200

    Use signed credential from vc-issuer

--- node-vc-embedded-signer/payloads.js
+++ node-vc-embedded-signer/payloads.js
@@ -35,48 +35,32 @@ export const unsignedCredential = {
 export const signedCredential = {
   "@context": [
     "https://www.w3.org/2018/credentials/v1",
-    "https://purl.imsglobal.org/spec/ob/v3p0/context/ob_v3p0.jsonld",
+    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.1.json",
     "https://w3id.org/security/data-integrity/v1"
   ],
-  "id": "http://example.com/credentials/3527",
-  "type": [
-    "VerifiableCredential",
-    "OpenBadgeCredential"
-  ],
-  "issuer": {
-    "id": "https://example.com/issuers/876543",
-    "type": [
-      "Profile"
-    ],
-    "url": "https://www.imsglobal.org",
-    "name": "Example Corp"
-  },
-  "issuanceDate": "2010-01-01T00:00:00Z",
-  "name": "Teamwork Badge",
+  "type": ["VerifiableCredential", "AchievementCredential"],
+  "id": "http://www.1edtech.org/ob/1",
+  "issuanceDate": "2022-11-27T00:00:00Z",
   "credentialSubject": {
-    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-    "type": [
-      "AchievementSubject"
-    ],
+    "type": "AchievementSubject",
+    "id": "did:1edtech:1",
     "achievement": {
-      "id": "https://example.com/achievements/21st-century-skills/teamwork",
-      "type": [
-        "Achievement"
-      ],
-      "criteria": {
-        "narrative": "Team members are nominated for this badge by their peers and recognized upon review by Example Corp management."
-      },
-      "description": "This badge recognizes the development of the capacity to collaborate within a group environment.",
-      "name": "Teamwork"
+      "id": "did:1edtech:achievement:1",
+      "type": ["Achievement"],
+      "criteria": { "narrative": "some narrative" },
+      "description": "some description",
+      "name": "some name"
     }
   },
+  "name": "Some name",
+  "issuer": { "id": "https://example.com/issuers/876543", "type": ["Profile"] },
   "proof": {
     "type": "DataIntegrityProof",
-    "created": "2010-01-01T19:23:24Z",
+    "created": "2023-05-29T08:42:26Z",
+    "proofPurpose": "assertionMethod",
     "verificationMethod": "https://example.com/issuers/876543#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi",
     "cryptosuite": "eddsa-2022",
-    "proofPurpose": "assertionMethod",
-    "proofValue": "z4qak22wvg7qwFSZtUdWbRUd7wCjU2VKKXU7NPuugBNYmeB6hHgdqUxChGxS33DUuUQmfgsjhdPkpfgg6zH9HzHdv"
+    "proofValue": "z2H1miRAGKPRCPUSudNTPrsWyvpRZzgtgjPBmnMkbcwR8StwEv8dr3NigviBgvZwBu8yA7wAyF5qLjyo3zN1UqLR2"
   }
 }
 
