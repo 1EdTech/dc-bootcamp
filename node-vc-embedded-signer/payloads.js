ae8a468bdf717932c302e9d84ca11af975ebfd31
Xavi Aracil <xaracil@1edtech.org>
2023-09-07 17:20:36 +0200

    added clr example

--- node-vc-embedded-signer/payloads.js
+++ node-vc-embedded-signer/payloads.js
@@ -32,6 +32,141 @@ export const unsignedCredential = {
   },
 };
 
+export const unsignedClr = {
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://purl.imsglobal.org/spec/clr/v2p0/context.json",
+    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json",
+    "https://purl.imsglobal.org/spec/ob/v3p0/extensions.json",
+    "https://w3id.org/security/data-integrity/v1"
+  ],
+  "id": "http://example.edu/credentials/3732",
+  "type": [
+    "VerifiableCredential",
+    "ClrCredential"
+  ],
+  "issuer": {
+    "id": "https://example.edu/issuers/565049",
+    "type": "Profile",
+    "name": "Example University"
+  },
+  "issuanceDate": "2010-01-01T00:00:00Z",
+  "name": "Sample Transcript",
+  "credentialSubject": {
+    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+    "type": "ClrSubject",
+    "verifiableCredential": [
+      {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json",
+          "https://purl.imsglobal.org/spec/ob/v3p0/extensions.json",
+          "https://w3id.org/security/data-integrity/v1"
+        ],
+        "id": "urn:uuid:91537dba-56cb-11ec-bf63-0242ac130002",
+        "type": [
+          "VerifiableCredential",
+          "AchievementCredential"
+        ],
+        "issuer": {
+          "id": "https://example.edu/issuers/565049",
+          "type": "Profile",
+          "name": "Example University"
+        },
+        "issuanceDate": "2010-01-01T00:00:00Z",
+        "name": "Example University Degree",
+        "credentialSubject": {
+          "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+          "type": "AchievementSubject",
+          "achievement": {
+            "id": "urn:uuid:a7467ef6-56cb-11ec-bf63-0242ac130002",
+            "type": "Achievement",
+            "creator": {
+              "id": "https://example.edu/issuers/565049",
+              "type": "Profile"
+            },
+            "name": "Achievement 1",
+            "criteria": {
+              "id": "https://example.edu/achievements/a7467ef6-56cb-11ec-bf63-0242ac130002/criteria"
+            },
+            "description": "Achievement 1",
+            "image": {
+              "id": "https://example.edu/achievements/sample.png",
+              "type": "Image"
+            }
+          }
+        },
+        "credentialSchema": [
+          {
+            "id": "https://purl.imsglobal.org/spec/ob/v3p0/schema/json/ob_v3p0_achievementcredential_schema.json",
+            "type": "1EdTechJsonSchemaValidator2019"
+          }
+        ],
+        "proof": [
+          {
+            "type": "DataIntegrityProof",
+            "created": "2023-07-25T13:17:33Z",
+            "verificationMethod": "https://example.edu/issuers/565049#z6MkvcpMB3ZXmLgSqDVtmcijHzpVikJFyfoSmCQBFe7j3vtD",
+            "cryptosuite": "eddsa-2022",
+            "proofPurpose": "assertionMethod",
+            "proofValue": "z4qj6AFEgstmd5b5brWYE7arn92snmPc8GTYEc3knDZSKGopHmXg71E27WknrazUnVa3nVfqugB6Q2sFbsjkgZngF"
+          }
+        ]
+      }
+    ],
+    "achievement": [
+      {
+        "id": "urn:uuid:a7467ef6-56cb-11ec-bf63-0242ac130002",
+        "type": "Achievement",
+        "creator": {
+          "id": "https://example.edu/issuers/565049",
+          "type": "Profile"
+        },
+        "name": "Achievement 1",
+        "criteria": {
+          "id": "https://example.edu/achievements/a7467ef6-56cb-11ec-bf63-0242ac130002/criteria"
+        },
+        "description": "Achievement 1",
+        "image": {
+          "id": "https://example.edu/achievements/sample.png",
+          "type": "Image"
+        }
+      },
+      {
+        "id": "urn:uuid:dd887f0a-56cb-11ec-bf63-0242ac130002",
+        "type": "Achievement",
+        "creator": {
+          "id": "https://example.edu/issuers/565049",
+          "type": "Profile"
+        },
+        "name": "Achievement 2",
+        "criteria": {
+          "id": "https://example.edu/achievements/dd887f0a-56cb-11ec-bf63-0242ac130002/criteria"
+        },
+        "description": "Achievement 2",
+        "image": {
+          "id": "https://example.edu/achievements/sample.png",
+          "type": "Image"
+        }
+      }
+    ],
+    "association": [
+      {
+        "type": "Association",
+        "associationType": "isParentOf",
+        "sourceId": "urn:uuid:a7467ef6-56cb-11ec-bf63-0242ac130002",
+        "targetId": "urn:uuid:dd887f0a-56cb-11ec-bf63-0242ac130002"
+      }
+    ]
+  },
+  "credentialSchema": [
+    {
+      "id": "https://purl.imsglobal.org/spec/clr/v2p0/schema/json/clr_v2p0_clrcredential_schema.json",
+      "type": "1EdTechJsonSchemaValidator2019"
+    }
+  ]
+}
+
 export const signedCredential = {
   "@context": [
     "https://www.w3.org/2018/credentials/v1",
