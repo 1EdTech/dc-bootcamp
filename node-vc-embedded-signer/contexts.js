53393708268fafc1bc46aec9d29134d52d270730
Xavi Aracil <xaracil@1edtech.org>
2024-03-21 14:39:13 +0100

    Added CLR context

--- node-vc-embedded-signer/contexts.js
+++ node-vc-embedded-signer/contexts.js
@@ -5,6 +5,7 @@ import context301  from './contexts/context-3.0.1.js';
 import context302  from './contexts/context-3.0.2.js';
 import context303  from './contexts/context-3.0.3.js';
 import contextClr  from './contexts/clrv2p0.js';
+import contextClr201  from './contexts/clrv2p0-2.0.1.js';
 import contextExtensions  from './contexts/extensions.js';
 
 const contexts = new Map();
@@ -14,6 +15,7 @@ contexts.set("https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.1.json", conte
 contexts.set("https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json", context302);
 contexts.set("https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json", context303);
 contexts.set("https://purl.imsglobal.org/spec/clr/v2p0/context.json", contextClr);
+contexts.set("https://purl.imsglobal.org/spec/clr/v2p0/context-2.0.1.json", contextClr201);
 contexts.set("https://purl.imsglobal.org/spec/ob/v3p0/extensions.json", contextExtensions);
 
 
