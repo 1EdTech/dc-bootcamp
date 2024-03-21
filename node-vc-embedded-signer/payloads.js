1854521069f7d77a7060d2b4a220f50fedf06326
Xavi Aracil <xaracil@1edtech.org>
2024-03-21 14:39:34 +0100

    Updated examples

--- node-vc-embedded-signer/payloads.js
+++ node-vc-embedded-signer/payloads.js
@@ -2,8 +2,8 @@
 
 export const unsignedCredential = {
   "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://purl.imsglobal.org/spec/ob/v3p0/context/ob_v3p0.jsonld",
+    "https://www.w3.org/ns/credentials/v2",
+    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json"
   ],
   id: "http://example.com/credentials/3527",
   type: ["VerifiableCredential", "OpenBadgeCredential"],
@@ -13,7 +13,7 @@ export const unsignedCredential = {
     url: "https://www.imsglobal.org",
     name: "Example Corp",
   },
-  issuanceDate: "2010-01-01T00:00:00Z",
+  validFrom: "2010-01-01T00:00:00Z",
   name: "Teamwork Badge",
   credentialSubject: {
     id: "did:example:ebfeb1f712ebc6f1c276e12ec21",
@@ -34,11 +34,10 @@ export const unsignedCredential = {
 
 export const unsignedClr = {
   "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://purl.imsglobal.org/spec/clr/v2p0/context.json",
-    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json",
-    "https://purl.imsglobal.org/spec/ob/v3p0/extensions.json",
-    "https://w3id.org/security/data-integrity/v1"
+    "https://www.w3.org/ns/credentials/v2",
+    "https://purl.imsglobal.org/spec/clr/v2p0/context-2.0.1.json",
+    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json",
+    "https://purl.imsglobal.org/spec/ob/v3p0/extensions.json"
   ],
   "id": "http://example.edu/credentials/3732",
   "type": [
@@ -50,7 +49,7 @@ export const unsignedClr = {
     "type": "Profile",
     "name": "Example University"
   },
-  "issuanceDate": "2010-01-01T00:00:00Z",
+  "validFrom": "2010-01-01T00:00:00Z",
   "name": "Sample Transcript",
   "credentialSubject": {
     "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
@@ -58,10 +57,9 @@ export const unsignedClr = {
     "verifiableCredential": [
       {
         "@context": [
-          "https://www.w3.org/2018/credentials/v1",
-          "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json",
-          "https://purl.imsglobal.org/spec/ob/v3p0/extensions.json",
-          "https://w3id.org/security/data-integrity/v1"
+          "https://www.w3.org/ns/credentials/v2",
+          "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json",
+          "https://purl.imsglobal.org/spec/ob/v3p0/extensions.json"
         ],
         "id": "urn:uuid:91537dba-56cb-11ec-bf63-0242ac130002",
         "type": [
@@ -73,7 +71,7 @@ export const unsignedClr = {
           "type": "Profile",
           "name": "Example University"
         },
-        "issuanceDate": "2010-01-01T00:00:00Z",
+        "validFrom": "2010-01-01T00:00:00Z",
         "name": "Example University Degree",
         "credentialSubject": {
           "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
@@ -108,7 +106,7 @@ export const unsignedClr = {
           "verificationMethod": "https://example.edu/issuers/565049#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi",
           "cryptosuite": "eddsa-rdfc-2022",
           "proofPurpose": "assertionMethod",
-          "proofValue": "z2CGNmCgEmN68CWch6Kgg4vnjRDE896jnUqfQtJoG11qxC8ntxUPCQaGckoHG7BXW7KWZyUiSs5EkKX3gEiGYKrz"
+          "proofValue": "z45QnLySMt2mWuW787G6cf3SLkP97ZkMZxLeXH5yaaTQPCtTup4GCV95tU8HEnQJSyGCokSj3AvUhmJtvveTmN4Vu"
         }
       }
     ],
@@ -167,33 +165,48 @@ export const unsignedClr = {
 
 export const signedCredential = {
   "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.1.json",
-    "https://w3id.org/security/data-integrity/v1"
+    "https://www.w3.org/ns/credentials/v2",
+    "https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.3.json"
   ],
-  "type": ["VerifiableCredential", "AchievementCredential"],
-  "id": "http://www.1edtech.org/ob/1",
-  "issuanceDate": "2022-11-27T00:00:00Z",
+  "id": "http://example.com/credentials/3527",
+  "type": [
+    "VerifiableCredential",
+    "OpenBadgeCredential"
+  ],
+  "issuer": {
+    "id": "https://1edtech.edu/issuers/565049",
+    "type": [
+      "Profile"
+    ],
+    "url": "https://www.imsglobal.org",
+    "name": "Example Corp"
+  },
+  "validFrom": "2010-01-01T00:00:00Z",
+  "name": "Teamwork Badge",
   "credentialSubject": {
-    "type": "AchievementSubject",
-    "id": "did:1edtech:1",
+    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+    "type": [
+      "AchievementSubject"
+    ],
     "achievement": {
-      "id": "did:1edtech:achievement:1",
-      "type": ["Achievement"],
-      "criteria": { "narrative": "some narrative" },
-      "description": "some description",
-      "name": "some name"
+      "id": "https://example.com/achievements/21st-century-skills/teamwork",
+      "type": [
+        "Achievement"
+      ],
+      "criteria": {
+        "narrative": "Team members are nominated for this badge by their peers and recognized upon review by Example Corp management."
+      },
+      "description": "This badge recognizes the development of the capacity to collaborate within a group environment.",
+      "name": "Teamwork"
     }
   },
-  "name": "Some name",
-  "issuer": { "id": "https://example.com/issuers/876543", "type": ["Profile"] },
   "proof": {
     "type": "DataIntegrityProof",
-    "created": "2023-05-29T08:42:26Z",
+    "created": "2010-01-01T19:23:24Z",
+    "verificationMethod": "https://1edtech.edu/issuers/565049#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi",
+    "cryptosuite": "eddsa-rdfc-2022",
     "proofPurpose": "assertionMethod",
-    "verificationMethod": "https://example.com/issuers/876543#z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi",
-    "cryptosuite": "eddsa-2022",
-    "proofValue": "z2H1miRAGKPRCPUSudNTPrsWyvpRZzgtgjPBmnMkbcwR8StwEv8dr3NigviBgvZwBu8yA7wAyF5qLjyo3zN1UqLR2"
+    "proofValue": "z65HUy8R54Yj1HijQ45h9AvVVdW8hs8YBaYsgCMaK97ABZ5zkEZSh5BLQC9rn4nrLmcXSycUTkPbYkebnbKUVacNL"
   }
 }
 
