bba08b7932615640df4bbf3d18813198207db0db
Xavi Aracil <xaracil@1edtech.org>
2024-03-21 14:19:13 +0100

    Moved to a dedicated folder

---
+++ node-vc-embedded-signer/kpi/kpi-accrediter.edu.js
@@ -0,0 +1,5 @@
+export const kpi = {
+    controller: 'https://accrediter.edu/issuers/565049',
+    publicKeyMultibase: 'z6MkqHLdLYHwKr169xzu9qvH1cq94pUjeQpfrPU5Xv3MtNzt',
+    secretKeyMultibase: 'zrv44e5jGnhd7TCnVgijKa4grTDb12YZstqDn72gwFuG5LjfTCgapr1xJFV3H7ugm56iPHPzPj87rbkoH2k535JxvfJ'
+};
