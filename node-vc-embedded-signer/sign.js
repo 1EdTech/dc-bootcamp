10667acdd6de90c49df79fe92e04dca5d26dd2ae
Xavi Aracil <xaracil@1edtech.org>
2024-11-14 09:37:42 -0500

    Refactor signing process to use ECDSA multikey and cryptosuite; added derived credential functionality

--- node-vc-embedded-signer/sign.js
+++ node-vc-embedded-signer/sign.js
@@ -6,9 +6,14 @@ import jsigs from 'jsonld-signatures';
 const {purposes: {AssertionProofPurpose}, extendContextLoader} = jsigs;
 
 // key and cryptosuite
-import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
-import {cryptosuite as eddsaRdfc2022CryptoSuite} from
-'@digitalbazaar/eddsa-rdfc-2022-cryptosuite';
+import * as EcdsaMultikey from '@digitalbazaar/ecdsa-multikey';
+import * as ecdsaSd2023Cryptosuite
+  from '@digitalbazaar/ecdsa-sd-2023-cryptosuite';
+const {createSignCryptosuite, createDiscloseCryptosuite} = ecdsaSd2023Cryptosuite;
+
+// import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
+// import {cryptosuite as eddsaRdfc2022CryptoSuite} from
+// '@digitalbazaar/eddsa-rdfc-2022-cryptosuite';
 
 // contexts
 import * as vcContexts from '@digitalbazaar/credentials-v2-context';
@@ -25,21 +30,24 @@ import { unsignedCredential, unsignedClr } from './payloads.js';
 const credentialToSign = unsignedCredential;
 
 // create the keypair to use when signing
-const keyPair = await Ed25519Multikey.from({
+const ecdsaMultikeyKeyPair = {
   '@context': 'https://w3id.org/security/multikey/v1',
   type: 'Multikey',
-  controller : kpi.controller,
+  controller: kpi.controller,
   id: `${kpi.controller}#${kpi.publicKeyMultibase}`,
   publicKeyMultibase: kpi.publicKeyMultibase,
   secretKeyMultibase: kpi.secretKeyMultibase
-});
+
+};
+const keyPair = await EcdsaMultikey.from({...ecdsaMultikeyKeyPair});
 
 console.log('Using multikey:', keyPair);
-console.log('Using public key (hex):', Buffer.from(keyPair.publicKey).toString('hex'));
-console.log('Using secret key (hex):', Buffer.from(keyPair.secretKey).toString('hex'));
+// console.log('Using public key (hex):', Buffer.from(keyPair.publicKey).toString('hex'));
+// console.log('Using secret key (hex):', Buffer.from(keyPair.secretKey).toString('hex'));
 
 const suite = new DataIntegrityProof({
-  signer: keyPair.signer(), cryptosuite: eddsaRdfc2022CryptoSuite
+  signer: keyPair.signer(),
+  cryptosuite: createSignCryptosuite()
 });
 suite.date = '2010-01-01T19:23:24Z';
 
@@ -93,15 +101,33 @@ const signedCredential = await issue({
 console.log('\nSigned credential');
 console.log(JSON.stringify(signedCredential, null, 2));
 
+// derived credential
+const derivedSuite = new DataIntegrityProof({
+  cryptosuite: createDiscloseCryptosuite({
+    selectivePointers: ['/credentialSubject/id', '/credentialSubject/achievement/name']
+  })
+});
+
+
+// create derived credential from the signed credential
+const derivedCredential = await jsigs.derive(signedCredential, {
+  suite: derivedSuite,
+  purpose: new AssertionProofPurpose(),
+  documentLoader
+});
+
+console.log('\nDerived credential');
+console.log(JSON.stringify(derivedCredential, null, 2));
+
 // verification
 
-const verSuite = new DataIntegrityProof({cryptosuite: eddsaRdfc2022CryptoSuite});
+// const verSuite = new DataIntegrityProof({cryptosuite: eddsaRdfc2022CryptoSuite});
 
-const verified = await verifyCredential({
-  credential: signedCredential,
-  documentLoader: documentLoader,
-  checkStatus: () => { return {verified: true}}, // simple status check
-  suite: [verSuite]
-})
+// const verified = await verifyCredential({
+//   credential: signedCredential,
+//   documentLoader: documentLoader,
+//   checkStatus: () => { return {verified: true}}, // simple status check
+//   suite: [verSuite]
+// })
 
-console.log(verified);
+// console.log(verified);
