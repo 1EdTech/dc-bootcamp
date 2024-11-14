fa5a9adf419fd05096280b000e71d3cff116a00e
Xavi Aracil <xaracil@1edtech.org>
2024-11-14 09:37:34 -0500

    Updated public and secret key multibase values in kpi-1edtech.js

--- node-vc-embedded-signer/kpi/kpi-1edtech.js
+++ node-vc-embedded-signer/kpi/kpi-1edtech.js
@@ -1,5 +1,7 @@
 export const kpi = {
     controller: 'https://1edtech.edu/issuers/565049',
-    publicKeyMultibase: 'z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi',
-    secretKeyMultibase: 'zrv2bqTbNwCTsRrHFcJCPjVAduh4Ezcnoq1A3ZxH1GWTNkxipLVuaAoMFmze2gFN9oNXfJjufxSHWVZzsJiUsMHFMcx'
+    // publicKeyMultibase: 'z6MkjZRZv3aez3r18pB1RBFJR1kwUVJ5jHt92JmQwXbd5hwi',
+    // secretKeyMultibase: 'zrv2bqTbNwCTsRrHFcJCPjVAduh4Ezcnoq1A3ZxH1GWTNkxipLVuaAoMFmze2gFN9oNXfJjufxSHWVZzsJiUsMHFMcx'
+    publicKeyMultibase: 'zDnaekGZTbQBerwcehBSXLqAg6s55hVEBms1zFy89VHXtJSa9',
+    secretKeyMultibase: 'z42tqZ5smVag3DtDhjY9YfVwTMyVHW6SCHJi2ZMrD23DGYS3'
 };
